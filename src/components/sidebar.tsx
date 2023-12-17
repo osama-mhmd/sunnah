9a8fc9a44f159aa519ac40b66e2291aeee1e7efe
= <=>
2023-12-17 03:37:32 +0200

    feat/installed-shadcn-ui-components

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/theme-toggle";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <div className="container p-3">
+        <ModeToggle />
+      </div>
+    </div>
+  );
+}
